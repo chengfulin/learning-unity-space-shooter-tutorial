e0262f5d3ca2bdd33e6d840f3129a8f0aa4f82b6
ChengFu Lin <chengfulin0806@gmail.com>
2019-08-25 17:51:52 +0800

    Modify player

---
+++ Assets/Scripts/OnEnterTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnEnterTrigger : MonoBehaviour
+{
+    [SerializeField] [Header("Target Tag Name")] private string targetTag = null;
+    [SerializeField] [Header("Callback")] private UnityEvent doStuff = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == targetTag && doStuff != null)
+        {
+            doStuff.Invoke();
+        }
+    }
+}
